54312c8ace536399f2a0bf2b61e8692abc2a41e8
Curtidor <tannermatos18@gmail.com>
2024-02-24 15:54:45 -0500

    code clean up

--- ConsoleGraphX/screen.h
+++ ConsoleGraphX/screen.h
@@ -10,27 +10,27 @@ class Screen
 private:
 	const short _m_width;
 	const short _m_height;
-	const short _m_pixel_width;
-	const short _m_pixel_height;
-	const short _m_debugger_height;
+	const short _m_pixelWidth;
+	const short _m_pixelHeight;
+	const short _m_debuggerHeight;
 	ScreenBuffer* _m_screenBuffer;
 	
-	static Screen* _s_active_screen;
+	static Screen* _s_activeScreen;
 
 
 public:
-	static const wchar_t pixel = L'█';
-	static const wchar_t noDrawChar = L'‎‎';
+	static const wchar_t s_pixel = L'█';
+	static const wchar_t s_transparentPixel = L'‎‎';
 
-	Screen(short width, short height, short debugger_height, short fontWidth, short fontHeight);
+	Screen(short width, short height, short debuggerHeight, short fontWidth, short fontHeight);
 	~Screen();
 
-	void SetPixel(int x, int y, CHAR_INFO pixel);
-	void SetPixels(CHAR_INFO* src_start, CHAR_INFO* src_end, CHAR_INFO* dest);
+	void SetPixel(int x, int y, CHAR_INFO s_pixel);
+	void SetPixels(CHAR_INFO* srcStart, CHAR_INFO* srcEnd, CHAR_INFO* dest);
 	void SetText(int x, int y, const std::string& text);
 	
-	static void SetPixel_A(int x, int y, CHAR_INFO pixel);
-	static void SetPixels_A(CHAR_INFO* src_start, CHAR_INFO* src_end, CHAR_INFO* dest);
+	static void SetPixel_A(int x, int y, CHAR_INFO s_pixel);
+	static void SetPixels_A(CHAR_INFO* srcStart, CHAR_INFO* srcEnd, CHAR_INFO* dest);
 	static void SetText_A(int x, int y, const std::string& text);
 
 	void SetCursorPosition(short x, short y);
