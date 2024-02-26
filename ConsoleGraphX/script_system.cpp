2c06f6551a7dc6775e54e0d9de68e757d4660718
Curtidor <tannermatos18@gmail.com>
2024-02-25 20:45:30 -0500

    code improvements and null checks

--- ConsoleGraphX/script_system.cpp
+++ ConsoleGraphX/script_system.cpp
@@ -4,17 +4,21 @@ std::unordered_set<Script*> ScriptSystem::_m_scripts;
 
 void ScriptSystem::Initialize() const 
 {
-	Dispatcher<Entity*>::RegisterListener("AddScript", RegisterScript);
-	Dispatcher<Entity*>::RegisterListener("RunTimeAddScript", RunTimeRegisterScript);
+	Dispatcher<Entity*>::RegisterListener("AddComponentStruct script", RegisterScript);
+	Dispatcher<Entity*>::RegisterListener("RemoveComponentStruct script", DeregisterScript);
 
+	Dispatcher<Entity*>::RegisterListener("RunTimeAddScript", RunTimeRegisterScript);
 	Dispatcher<Entity*>::RegisterListener("RemoveScript", DeregisterScript);
 }
 
 void ScriptSystem::Update(float delta_time) const
 {
-	for (Script* entity : _m_scripts)
+	for (Script* script : _m_scripts)
 	{
-		entity->Update();
+		if (script->GetOwner() == NULL)
+			continue;
+
+		script->Update();
 	}
 }
 
@@ -39,7 +43,7 @@ void ScriptSystem::StartScript(Script* script)
 
 void ScriptSystem::RegisterScript(Entity* entity)
 {
-	if(Script* script = (Script*)entity->GetComponentByID(ComponentID::script))
+	if( Script* script = (Script*)entity->GetComponentByID(ComponentID::script))
 	{
 		_m_scripts.insert(script);
 	}
@@ -64,7 +68,7 @@ void ScriptSystem::DeregisterScript(Entity* entity)
 }
 
 
-
+// DeRegister script using script pointer (With Script, WS)
 void ScriptSystem::DeregisterScriptWS(Script* script)
 {
 	auto it = _m_scripts.find(script);
