f58b7a463752bcccf2291b916ab252eaf2d56e28
Curtidor <tannermatos18@gmail.com>
2024-02-25 20:44:42 -0500

    added support for deleting entities from a scene

--- ConsoleGraphX/scene_system.cpp
+++ ConsoleGraphX/scene_system.cpp
@@ -1,19 +1,22 @@
 #include "scene_system.h"
 
-Scene* SceneSystem::_s_active_scene;
+Scene* SceneSystem::_s_activeScene;
 std::unordered_map<std::string, Scene*> SceneSystem::_s_scenes;
 
 void SceneSystem::RegisterScene(Scene* scene)
 {
     _s_scenes[scene->GetSceneName()] = scene;
+
+    Dispatcher<Entity*>::RegisterListener("EntityDeletionEvent", std::bind(&Scene::DeregisterEntity, scene, std::placeholders::_1));
+
 }
 
 void SceneSystem::DeregisterScene(const std::string& name)
 {
     auto it = _s_scenes.find(name);
 
-    if (name == _s_active_scene->GetSceneName())
-        _s_active_scene = nullptr;
+    if (name == _s_activeScene->GetSceneName())
+        _s_activeScene = nullptr;
 
     DeleteScene(name);
     if (it != _s_scenes.end())
@@ -28,8 +31,8 @@ void SceneSystem::LoadScene(const std::string& name)
     {
         throw std::runtime_error("Scene must be registered");
     }
-    _s_active_scene = _s_scenes[name];
-    _s_active_scene->Initialize();
+    _s_activeScene = _s_scenes[name];
+    _s_activeScene->Initialize();
 }
 
 void SceneSystem::DeleteScene(const std::string& name)
@@ -48,6 +51,11 @@ void SceneSystem::DeleteScene(const std::string& name)
 bool SceneSystem::IsSceneRegistered(const std::string& name)
 {
     return _s_scenes.find(name) != _s_scenes.end();
+}
+
+Scene* SceneSystem::GetActiveScene()
+{
+    return _s_activeScene;
 }
 
 const std::unordered_map<std::string, Scene*>& SceneSystem::GetScenes()
