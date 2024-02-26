22a157e34b064c8e0934978be0e6c56af6b7ad6c
Curtidor <tannermatos18@gmail.com>
2024-02-25 20:46:18 -0500

    fixed comparator returning incorrect results

--- ConsoleGraphX/sprite_system.cpp
+++ ConsoleGraphX/sprite_system.cpp
@@ -11,10 +11,16 @@ bool SpriteComparator::operator()(const Entity* entityAC, const Entity* entityBC
 		throw std::runtime_error("Null sprite encountered in SpriteComparator");
 	}
 
-	return spriteA->m_layer < spriteB->m_layer;
+	if (spriteA->m_layer != spriteB->m_layer) {
+		return spriteA->m_layer < spriteB->m_layer;
+	}
+	else {
+		// Use memory addresses as tie-breaker
+		return entityA < entityB;
+	}
 }
 
-std::multiset<Entity*, SpriteComparator> SpriteSystem::_s_entities_w_sprites;
+std::multiset<Entity*, SpriteComparator> SpriteSystem::_s_entitySprites;
 
 void SpriteSystem::Initialize() const
 {
@@ -30,15 +36,15 @@ void SpriteSystem::Update(float delta_time) const
 
 void SpriteSystem::RegisterEntitySprite(Entity* entity)
 {
-	_s_entities_w_sprites.insert(entity);
+	_s_entitySprites.insert(entity);
 }
 
 void SpriteSystem::DeregisterEntitySprite(Entity* entity)
 {
-	auto it = _s_entities_w_sprites.find(entity);
+	auto it = _s_entitySprites.find(entity);
 
-	if (it != _s_entities_w_sprites.end()) {
-		_s_entities_w_sprites.erase(it);
+	if (it != _s_entitySprites.end()) {
+		_s_entitySprites.erase(it);
 	}
 }
 
@@ -46,9 +52,9 @@ void SpriteSystem::DeregisterEntitySprite(Entity* entity)
 std::vector<Entity*> SpriteSystem::GetEntitySprites()
 {
 	std::vector<Entity*> entitiesVector;
-	entitiesVector.reserve(_s_entities_w_sprites.size()); 
+	entitiesVector.reserve(_s_entitySprites.size()); 
 
-	entitiesVector.insert(entitiesVector.end(), _s_entities_w_sprites.begin(), _s_entities_w_sprites.end());
+	entitiesVector.insert(entitiesVector.end(), _s_entitySprites.begin(), _s_entitySprites.end());
 
 	return entitiesVector;
 }
