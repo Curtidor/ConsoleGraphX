7f27f3223dce8a424c1b22720881511c0da1882d
Curtidor <tannermatos18@gmail.com>
2024-03-01 17:36:23 -0500

    changed m_isEnabled to _m_isEnabled

--- ConsoleGraphX/script.h
+++ ConsoleGraphX/script.h
@@ -6,16 +6,19 @@
 struct Script : public Component
 {
 protected:
-	bool m_is_enabled;
+	bool _m_isEnabled;
 
 public:
 	Script();
+	Script(const Script& script);
 
 	virtual void Awake(Entity* owner);
 	virtual void Start(Entity* owner);
 	virtual void Update(Entity* owner);
 
 	bool IsEnabled();
+	
+	void SetState(bool state);
 
 	Component* Clone() const override;
 	int GetID() const override;
