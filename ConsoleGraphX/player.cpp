73a4b03389d33a9b35f0281227dacd2aba79ee88
Curtidor <tannermatos18@gmail.com>
2024-03-01 17:28:41 -0500

    sprite layer change

--- ConsoleGraphX/player.cpp
+++ ConsoleGraphX/player.cpp
@@ -6,11 +6,9 @@ Player::Player() : Script()
 void Player::Awake(Entity* owner)
 {
 	Sprite* sprite = (Sprite*)owner->AddComponent<Sprite>(7, 12, DarkYellow);
-	sprite->m_layer = 1;
+	sprite->m_layer = 4;
 	owner->GetComponent<Transform>()->SetPosition(22, 78);
 	owner->AddComponent<PlayerController>(25, 5, 9);
-
-	this->m_is_enabled = true;
 }
 
 void Player::Update(Entity* owner)
