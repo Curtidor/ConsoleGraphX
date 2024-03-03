69b2df9cae0b72e55a8b8ac3648c9774776610b4
Curtidor <tannermatos18@gmail.com>
2024-03-03 06:48:49 -0500

    changed log message to runtime error

--- ConsoleGraphX/script_system.cpp
+++ ConsoleGraphX/script_system.cpp
@@ -2,6 +2,7 @@
 
 std::unordered_set<Entity*> ScriptSystem::_s_scripts;
 
+// TODO when script state changes we should stop calling update on that script or start calling update deping on the state
 
 void ScriptSystem::Initialize() const 
 {
@@ -17,24 +18,14 @@ void ScriptSystem::Update(float delta_time) const
 	for (Entity* entity : _s_scripts)
 	{
 		if (entity == nullptr)
-		{
-			Debugger::S_LogMessage("[SCRIPT SYSTEM] update, null entity pointer");
-			continue;
-		}
+			throw std::runtime_error("[SCRIPT SYSTEM] update, null entity pointer");
 
 		for (Component* component : entity->GetScripts())
 		{
 			Script* script = static_cast<Script*>(component);
-			if (script == nullptr)
-			{
-				Debugger::S_LogMessage("[SCRIPT SYSTEM] update, null script pointer");
-				continue;
-			}
 
 			if (script->IsEnabled())
-			{
 				script->Update(entity);
-			}
 		}
 
 	}
@@ -42,14 +33,16 @@ void ScriptSystem::Update(float delta_time) const
 
 void ScriptSystem::_DoScriptWarmUp(Entity* entity)
 {
-	for (Component* component : entity->GetScripts())
+	std::unordered_set<Component*> scripts = entity->GetScripts();
+
+	for (Component* component : scripts)
 	{
 		Script* script = static_cast<Script*>(component);
 
 		script->Awake(entity);
 	}
 
-	for (Component* component : entity->GetScripts())
+	for (Component* component : scripts)
 	{
 		Script* script = static_cast<Script*>(component);
 
