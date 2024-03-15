a6dcbaf1ac559f5a482cc364086172412ca8ae6e
Curtidor <tannermatos18@gmail.com>
2024-03-15 13:41:48 -0400

    camera system

---
+++ ConsoleGraphX/camera_system.cpp
@@ -0,0 +1,109 @@
+#include "camera_system.h"
+#include <unordered_set>
+#include <stdexcept>
+#include "camera.h"
+#include "debugger.h"
+#include "dispatcher.h"
+#include "entity.h"
+#include "screen.h"
+#include "vector3.h"
+
+std::unordered_set<Camera*> CameraSystem::_s_backupCamera;
+std::unordered_set<Camera*> CameraSystem::_s_cameras;
+std::unordered_set<Camera*> CameraSystem::_s_activeCameras;
+
+void CameraSystem::Initialize() const
+{
+	Dispatcher<Entity*>::RegisterListener("RemoveComponentstruct Camera", _CameraRemovedHandler);
+	Camera* cam = new Camera(Screen::GetWidth_A(), Screen::GetHeight_A(), Vector3());
+
+	_s_backupCamera.insert(cam);
+
+}
+
+void CameraSystem::Update(float deltaTime) const
+{}
+
+void CameraSystem::_AddCameraToActive(Camera* cam)
+{
+	if (cam == nullptr)
+		throw std::runtime_error("bad camera pointer, camera system");
+
+	_s_activeCameras.insert(cam);
+}
+
+void CameraSystem::_RemoveCameraFromActive(Camera* cam)
+{
+	if (cam == nullptr)
+		throw std::runtime_error("bad camera pointer, camera system");
+
+	auto it = _s_activeCameras.find(cam);
+
+	if (it != _s_activeCameras.end())
+		_s_activeCameras.erase(it);
+}
+
+void CameraSystem::_CameraRemovedHandler(Entity* entity)
+{
+	if (entity == nullptr)
+		throw std::runtime_error("null entity, cam system");
+
+	Camera* cam = entity->GetComponent<Camera>();
+	if (cam == nullptr)
+		return;
+
+	CameraSystem::DeregisterCamera(cam);
+}
+
+void CameraSystem::RegisterCamera(Camera* cam)
+{
+	if (cam == nullptr)
+		throw std::runtime_error("bad camera pointer, camera system");
+
+
+	_s_cameras.insert(cam);
+}
+
+void CameraSystem::DeregisterCamera(Camera* cam)
+{
+	if (cam == nullptr)
+		throw std::runtime_error("bad camera pointer, camera system");
+
+	auto itCam = _s_cameras.find(cam);
+
+	if (itCam != _s_cameras.end())
+		_s_cameras.erase(itCam);
+
+	_RemoveCameraFromActive(cam);
+}
+
+void CameraSystem::SetCameraState(Camera* cam, bool state)
+{
+	if (cam == nullptr)
+		throw std::runtime_error("bad camera pointer, camera system");
+
+	auto itCam = _s_cameras.find(cam);
+
+	if (itCam == _s_cameras.end())
+	{
+		Debugger::S_LogMessage("When using set camera state, the camera must be registered already");
+		return;
+	}
+	
+	if (state)
+		_AddCameraToActive(cam);
+	else
+		_RemoveCameraFromActive(cam);
+}
+
+const std::unordered_set<Camera*> CameraSystem::GetActiveCameras()
+{
+	if (_s_activeCameras.size() == 0)
+	{
+		Debugger::S_LogMessage("[CAMERA SYSTEM], using backup camera!");
+		return _s_backupCamera;
+	}
+
+	return _s_activeCameras;
+}
+
---
+++ ConsoleGraphX/camera_system.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "camera.h"
+#include <unordered_set>
+#include "system.h"
+#include "entity.h"
+
+class CameraSystem : public System
+{
+private:
+	static std::unordered_set<Camera*> _s_backupCamera;
+	static std::unordered_set<Camera*> _s_cameras;
+	static std::unordered_set<Camera*> _s_activeCameras;
+
+	static void _RemoveCameraFromActive(Camera* cam);
+	static void _CameraRemovedHandler(Entity* entity);
+	static void _AddCameraToActive(Camera* cam);
+
+
+public:
+	void Initialize() const override;
+	void Update(float delta_time) const override;
+
+	static void RegisterCamera(Camera* cam);
+	static void DeregisterCamera(Camera* cam);
+
+	static void SetCameraState(Camera* cam, bool state);
+
+	const static std::unordered_set<Camera*> GetActiveCameras();
+	
+};
