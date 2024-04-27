fe4699e09df9eaec4d4b4635e3f38c4f898a44cc
Curtidor <tannermatos18@gmail.com>
2024-04-27 16:04:28 -0400

    camera now follows the player in the y direction

--- ConsoleGraphX/player.cpp
+++ ConsoleGraphX/player.cpp
@@ -18,7 +18,7 @@ void Player::Awake(Entity* owner)
 	Sprite* sprite = (Sprite*)owner->AddComponent<Sprite>(7, 12, DarkYellow);
 	sprite->m_layer = 10;
 
-	owner->GetComponent<Transform>()->SetPosition(0,75);
+	owner->GetComponent<Transform>()->SetPosition(0,40);
 	owner->AddComponent<PlayerController>(25, 5, 9);
 
 	_m_playerCam = owner->GetComponent<Camera>();
@@ -29,8 +29,8 @@ void Player::Update(Entity* owner)
 	Vector3 playerPosition = owner->GetPosition();
 	Vector3 camPostion = _m_playerCam->GetPosition();
 
-	//_m_playerCam->SetPosition(Vector3(playerPosition.x - _m_playerCam->GetWidth() * 0.5f, playerPosition.y + _m_playerCam->GetHeight() * 0.5f));
-	_m_playerCam->SetPosition(Vector3(playerPosition.x - _m_playerCam->GetWidth() * 0.5f, 0));
+	_m_playerCam->SetPosition(Vector3(playerPosition.x - _m_playerCam->GetWidth() * 0.5f, playerPosition.y - _m_playerCam->GetHeight() * 0.5f));
+	//_m_playerCam->SetPosition(Vector3(playerPosition.x - _m_playerCam->GetWidth() * 0.5f, 0));
 
 	Debugger::S_LogMessage("player x: " + std::to_string(playerPosition.x) + " y: " + std::to_string(playerPosition.y));
 	Debugger::S_LogMessage("cam x: " + std::to_string(camPostion.x) + " y: " + std::to_string(camPostion.y));
