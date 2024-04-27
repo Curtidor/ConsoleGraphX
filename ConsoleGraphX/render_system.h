215977ae0184b9ca69edd13ed648ca330b5fd01e
Curtidor <tannermatos18@gmail.com>
2024-04-27 16:04:09 -0400

    add documentation to functions that are currently in use as well as renamed a few methods

--- ConsoleGraphX/render_system.h
+++ ConsoleGraphX/render_system.h
@@ -18,14 +18,38 @@ private:
 		int bottom;
 	};
 
-	static bool _IsEntityNotInCamView(const _OverlapPoints& overlapPoints , const Vector2& spriteSize);
-	static void _CalculateEntityOverLapCamera(const Vector3& entityPosition, const Vector3& camPosition, const Vector2& viewPortSize, Sprite* sprite, _OverlapPoints& overlapPoints);
+	/**
+	 * @brief Checks if an entity is visible in the camera's view.
+	 * @param overlapPoints The overlap points of the entity's sprite with the camera's view.
+	 * @param spriteSize The size of the sprite.
+	 * @return True if the entity is visible in the camera's view, false otherwise.
+	 */
+	static bool _IsEntityVisibleInView(const _OverlapPoints& overlapPoints, const Vector2& spriteSize);
+
+	/**
+	 * @brief Calculates the overlap of the entity's sprite with the camera's view.
+	 * @param entityPosition The position of the entity.
+	 * @param camPosition The position of the camera.
+	 * @param viewPortSize The size of the viewport.
+	 * @param sprite Pointer to the sprite component of the entity.
+	 * @param overlapPoints Reference to store the calculated overlap points.
+	 */
+	static void _CalculateEntityOverlapWithCamera(const Vector3& entityPosition, const Vector3& camPosition, const Vector2& viewPortSize, Sprite* sprite, _OverlapPoints& overlapPoints);
+	/**
+	 * @brief Draws a sprite onto the screen buffer within the camera's view.
+	 * @param relEntityPosition The relative position of the entity within the camera's view.
+	 * @param sprite Pointer to the sprite to be drawn.
+	 * @param overlapPoints The overlap points of the sprite with the camera's view.
+	 */
+	static void _DrawSprite_SS(const Vector3& relEntityPosition, Sprite* sprite, const _OverlapPoints& overlapPoints);
+	static void _DrawSprite_SP(const Vector3& relEntityPosition, Sprite* sprite, const _OverlapPoints& overlapPoints);
 
 public:
+	/**
+	 * @brief Draws sprites of entities within the camera's view.
+	 * @param entities Vector of pointers to entities to be drawn.
+	 */
 	static void DrawSprites(const std::vector<Entity*>& entities);
-
-	static void DrawSprite_SS(const Vector3& relEntityPosition, Sprite* sprite, const _OverlapPoints& overlapPoints);
-	static void DrawSprite_SP(const Vector3& relEntityPosition, Sprite* sprite, const _OverlapPoints& overlapPoints);
 
 	static void DrawLine(Vector2 origin, Vector2 end, int color);
 };
