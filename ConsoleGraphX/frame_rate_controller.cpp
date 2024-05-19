fda2f34cdfd3a5a887110bc3f15da31bddba741b
Curtidor <tannermatos18@gmail.com>
2024-05-19 20:56:44 -0300

    frame rate controller

---
+++ ConsoleGraphX/frame_rate_controller.cpp
@@ -0,0 +1,98 @@
+#include <chrono>
+#include "frame_rate_controller.h"
+#include <thread>
+
+namespace ConsoleGraphX
+{
+	FrameRateController::FrameRateController()
+	{
+		_m_prevCaptureTime = std::chrono::high_resolution_clock::now();
+		_m_currentCaptureTime = std::chrono::high_resolution_clock::now();
+
+		_m_deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(_m_currentCaptureTime - _m_prevCaptureTime);
+		_m_deltaTimeNS = std::chrono::duration_cast<std::chrono::nanoseconds>(_m_currentCaptureTime - _m_prevCaptureTime);
+		_m_elapsedTime = std::chrono::milliseconds(0);
+
+		_m_frameCount = 0;
+		_m_framesPerSecond = 0;
+		_m_targetFrameRate = 60;
+		_m_targetFrameDuration = std::chrono::milliseconds(1000 / _m_targetFrameRate);
+	}
+
+	FrameRateController::FrameRateController(int targetFrameRate)
+	{
+		_m_prevCaptureTime = std::chrono::high_resolution_clock::now();
+		_m_currentCaptureTime = std::chrono::high_resolution_clock::now();
+
+		_m_deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(_m_currentCaptureTime - _m_prevCaptureTime);
+		_m_deltaTimeNS = std::chrono::duration_cast<std::chrono::nanoseconds>(_m_currentCaptureTime - _m_prevCaptureTime);
+		_m_elapsedTime = std::chrono::milliseconds(0);
+
+		_m_frameCount = 0;
+		_m_framesPerSecond = 0;
+		_m_targetFrameRate = targetFrameRate;
+		_m_targetFrameDuration = std::chrono::milliseconds(1000 / _m_targetFrameRate);
+	}
+
+	void FrameRateController::CaptureFrame()
+	{
+		_m_currentCaptureTime = std::chrono::high_resolution_clock::now();
+
+		_m_deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(_m_currentCaptureTime - _m_prevCaptureTime);
+		_m_deltaTimeNS = _m_currentCaptureTime - _m_prevCaptureTime;
+
+		_m_elapsedTime += _m_deltaTime;
+
+		_m_prevCaptureTime = _m_currentCaptureTime;
+
+		_m_frameCount++;
+	}
+
+	void FrameRateController::StartCapture()
+	{
+		_m_prevCaptureTime = std::chrono::high_resolution_clock::now();
+	}
+
+	void FrameRateController::EndCapture()
+	{
+		_m_currentCaptureTime = std::chrono::high_resolution_clock::now();
+
+		_m_deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(_m_currentCaptureTime - _m_prevCaptureTime);
+		_m_deltaTimeNS = _m_currentCaptureTime - _m_prevCaptureTime;
+	}
+
+	void FrameRateController::WaitForNextFrame(std::chrono::milliseconds deltaTime)
+	{
+		if (deltaTime <= _m_targetFrameDuration)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(_m_targetFrameDuration - deltaTime));
+		}
+
+		if (_m_elapsedTime >= std::chrono::milliseconds(1000))
+		{
+			_m_framesPerSecond = _m_frameCount;
+			_m_frameCount = 0;
+			_m_elapsedTime = std::chrono::milliseconds(0);
+		}
+	}
+
+	void FrameRateController::ResetFrameCount()
+	{
+		_m_frameCount = 0;
+	}
+
+	int FrameRateController::GetFramesPerSecond() const
+	{
+		return _m_framesPerSecond;
+	}
+
+	std::chrono::nanoseconds FrameRateController::GetDeltaTimeNS() const
+	{
+		return _m_deltaTimeNS;
+	}
+
+	std::chrono::milliseconds FrameRateController::GetDeltaTimeMS() const
+	{
+		return _m_deltaTime;
+	}
+}
---
+++ ConsoleGraphX/frame_rate_controller.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <chrono>
+
+namespace ConsoleGraphX
+{
+    class FrameRateController
+    {
+    private:
+        std::chrono::high_resolution_clock::time_point _m_prevCaptureTime;
+        std::chrono::high_resolution_clock::time_point _m_currentCaptureTime;
+        std::chrono::milliseconds _m_deltaTime;
+        std::chrono::nanoseconds _m_deltaTimeNS;
+        std::chrono::milliseconds _m_elapsedTime;
+
+        int _m_frameCount;
+        int _m_framesPerSecond;
+        int _m_targetFrameRate;
+        std::chrono::milliseconds _m_targetFrameDuration;
+
+    public:
+        FrameRateController();
+        FrameRateController(int targetFrameRate);
+
+        void CaptureFrame();
+        void StartCapture();
+        void EndCapture();
+        void WaitForNextFrame(std::chrono::milliseconds deltaTime);
+        void ResetFrameCount();
+
+        int GetFramesPerSecond() const;
+        std::chrono::nanoseconds GetDeltaTimeNS() const;
+        std::chrono::milliseconds GetDeltaTimeMS() const;
+    };
+}
