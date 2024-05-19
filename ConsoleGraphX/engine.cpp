e0af5ec23f5b6302f483b71592369e6ae5c18fdd
Curtidor <tannermatos18@gmail.com>
2024-05-19 20:49:36 -0300

    organizes system management into a dedicated SystemManager class, integrates a FrameRateController for frame rate control, refactors update logic into _Update for clarity, and adjusts the InitializeEngine function to accept a debugger reference.

--- ConsoleGraphX/engine.cpp
+++ ConsoleGraphX/engine.cpp
@@ -1,7 +1,8 @@
-#include "engine.h"
-#include <chrono>
 #include <string>
+#include "engine.h"
+#include "frame_rate_controller.h"
 #include "debugger.h"
+#include "system_manager.h"
 #include "input_system.h"
 #include "player_controller_system.h"
 #include "render_system.h"
@@ -10,104 +11,79 @@
 #include "script_system.h"
 #include "sprtite_system.h"
 #include "camera_system.h"
-#include "system.h"
 
 namespace ConsoleGraphX
 {
-    System* Engine::_m_system = nullptr;
-    ConsoleGraphX_Interal::Debugger* Engine::_m_debugger = nullptr;
+    ConsoleGraphX_Interal::SystemManager* Engine::_s_systemManager = nullptr;
+    ConsoleGraphX_Interal::Debugger* Engine::_s_debugger = nullptr;
 
-    // Define the variables for delta time calculation
-    std::chrono::high_resolution_clock::time_point Engine::_m_prev_frame_time;
-    std::chrono::high_resolution_clock::time_point Engine::_m_current_frame_time;
+    bool Engine::_s_isRunning = false;
 
-    float Engine::_m_delta_time = 0;
-    bool Engine::_m_is_running = false;
-
-    void Engine::InitializeEngine(short screen_width, short screen_height, short pixel_width, short pixel_height, ConsoleGraphX_Interal::Debugger* debugger)
+    void Engine::InitializeEngine(short screen_width, short screen_height, short pixel_width, short pixel_height, ConsoleGraphX_Interal::Debugger& debugger)
     {
-        int debugger_height = debugger != nullptr ? debugger->GetMaxMessages() : 0;
+        _s_debugger = &debugger;
+
+        int debugger_height = _s_debugger->GetMaxMessages();
 
         ConsoleGraphX_Interal::Screen* screen = new ConsoleGraphX_Interal::Screen(screen_width, screen_height, debugger_height, pixel_width, pixel_height);
-        screen->MemFillScreen( 0 );
-
-        _m_debugger = debugger;
-
-        _m_system = new System();
-        _m_system->RegisterSystem<ScriptSystem>();
-        _m_system->RegisterSystem<ConsoleGraphX_Interal::SpriteSystem>();
-        _m_system->RegisterSystem<PlayerControllerSystem>();
-        _m_system->RegisterSystem<CameraSystem>();
-
-        // Initialize the previous frame time to the current time
-        _m_prev_frame_time = std::chrono::high_resolution_clock::now();
+        ConsoleGraphX_Interal::Screen::SetActiveScreen_A(screen);
+        
+        _s_systemManager = new ConsoleGraphX_Interal::SystemManager();
+        _s_systemManager->RegisterSystem<ScriptSystem>();
+        _s_systemManager->RegisterSystem<ConsoleGraphX_Interal::SpriteSystem>();
+        _s_systemManager->RegisterSystem<PlayerControllerSystem>();
+        _s_systemManager->RegisterSystem<CameraSystem>();
     }
 
     void Engine::Run()
     {
-        if (_m_is_running)
+        if (_s_isRunning)
             return;
 
-        // Initialize all the scripts
         ScriptSystem::WarmUp();
 
         ConsoleGraphX_Interal::Screen* active_screen = ConsoleGraphX_Interal::Screen::GetActiveScreen_A();
-        while (true)
-        {
-            _m_current_frame_time = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<float> frame_duration = _m_current_frame_time - _m_prev_frame_time;
-
-            _m_delta_time = frame_duration.count() * 10;
-            _m_prev_frame_time = _m_current_frame_time;
-
-            InputSystem::GetPressedKeys();
-            InputSystem::UpdateMousePosition();
-
-            // kill entity event happens here
-            _m_system->Update(_m_delta_time);
-
-            // entity is still alive here, but in processes list of entities to kill
-            ConsoleGraphX_Interal::RenderSystem::DrawSprites(ConsoleGraphX_Interal::SpriteSystem::GetEntitySprites());
-
-            // entity is killed here
-            SceneSystem::GetActiveScene()->DeleteEntities();
-
-            if (_m_debugger != nullptr)
-                _m_debugger->DisplayMessages();
+      
+        FrameRateController frameRateController(60);
+        FrameRateController updateTimer;
+        std::chrono::nanoseconds deltaTime(0);
 
 
-            UpdateFPS();
+        // NOTE THIS TIMING CODE IS A PLACE HOLDER AND IS HERE FOR TESTING
+        while (true) {
+            frameRateController.CaptureFrame();  // Capture start time of the frame
+            updateTimer.StartCapture();  // Start timing the update and draw operations
+            
+            deltaTime = frameRateController.GetDeltaTimeMS();
+            
+            _Update(1.0f/60);  // Update with delta time in seconds
 
             active_screen->DrawScreen();
-            active_screen->MemFillScreen(0 );
+            active_screen->MemFillScreen(0);
 
-            //Sleep(4);
+            active_screen->SetConsoleName("FPS " + std::to_string(frameRateController.GetFramesPerSecond()));
+
+            updateTimer.EndCapture();  // End timing the update and draw operations
+
+            deltaTime = updateTimer.GetDeltaTimeMS();  // Get the time taken for update and draw operations
+
+            frameRateController.WaitForNextFrame(std::chrono::duration_cast<std::chrono::milliseconds>(deltaTime + frameRateController.GetDeltaTimeMS()));  // Wait for the next frame if needed
         }
+
     }
 
-    void Engine::UpdateFPS()
+    void Engine::_Update(float deltaTime)
     {
-        static int frame_count = 0;
-        static auto last_time = std::chrono::high_resolution_clock::now();
+        InputSystem::GetPressedKeys();
+        InputSystem::UpdateMousePosition();
 
-        frame_count++;
-        auto current_time = std::chrono::high_resolution_clock::now();
-        auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(current_time - last_time);
+        _s_systemManager->Update(deltaTime);
 
-        if (elapsed_time.count() >= 1)
-        {
-            int fps = frame_count;
-            frame_count = 0;
-            last_time = current_time;
+        ConsoleGraphX_Interal::RenderSystem::DrawSprites(ConsoleGraphX_Interal::SpriteSystem::GetEntitySprites());
 
-            ConsoleGraphX_Interal::Screen* active_screen = ConsoleGraphX_Interal::Screen::GetActiveScreen_A();
-            active_screen->SetConsoleName("FPS: " + std::to_string(fps));
-        }
-    }
+        SceneSystem::GetActiveScene()->DeleteEntities();
 
-    const float Engine::DeltaTime()
-    {
-        return Engine::_m_delta_time;
+        _s_debugger->DisplayMessages();
     }
 };
 
--- ConsoleGraphX/engine.h
+++ ConsoleGraphX/engine.h
@@ -1,29 +1,22 @@
 #pragma once
-#include <chrono>
-// utilities
 #include "debugger.h"
-// systems
-#include "system.h"
+#include "system_manager.h"
 
 namespace ConsoleGraphX
 {
 	class Engine
 	{
 	private:
-		static System* _m_system;
-		static ConsoleGraphX_Interal::Debugger* _m_debugger;
+		static ConsoleGraphX_Interal::SystemManager* _s_systemManager;
+		static ConsoleGraphX_Interal::Debugger* _s_debugger;
 
-		static float _m_delta_time;
-		static bool _m_is_running;
+		static bool _s_isRunning;
 
-		static std::chrono::high_resolution_clock::time_point _m_prev_frame_time;
-		static std::chrono::high_resolution_clock::time_point _m_current_frame_time;
+		static void _Update(float deltaTime);
+
 
 	public:
-		static void InitializeEngine(short screen_width, short screen_height, short pixel_width, short pixel_height, ConsoleGraphX_Interal::Debugger* debugger);
+		static void InitializeEngine(short screen_width, short screen_height, short pixel_width, short pixel_height, ConsoleGraphX_Interal::Debugger& debugger);
 		static void Run();
-
-		const static float DeltaTime();
-		static void UpdateFPS();
 	};
 };
