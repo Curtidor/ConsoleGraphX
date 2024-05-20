c75b6cf67a7539f4e3fba8d7e9aea3bb6acb1eeb
Curtidor <tannermatos18@gmail.com>
2024-05-19 21:05:59 -0300

    added a deconstructor

--- ConsoleGraphX/scene.cpp
+++ ConsoleGraphX/scene.cpp
@@ -1,15 +1,21 @@
-#include "scene.h"
 #include <string>
 #include <unordered_set>
-#include "component.h"
+#include "scene.h"
 #include "entity.h"
-#include "script.h"
 
 namespace ConsoleGraphX
 {
     Scene::Scene(std::string name)
         : _m_scene_name(name)
     {}
+
+    Scene::~Scene()
+    {
+        _m_entitiesToDelete.insert(_m_entities.begin(), _m_entities.end());
+
+        // DeleteEntities also removes the entity from _m_entities
+        DeleteEntities();
+    }
 
     Entity* Scene::RegisterEntityN(std::string name)
     {
@@ -38,22 +44,26 @@ namespace ConsoleGraphX
 
             if (it != _m_entities.end())
             {
-                for (auto& componentPair : entity->GetComponents())
+                auto& components = entity->GetComponents();
+                for (auto itComponents = components.begin(); itComponents != components.end();)
                 {
-                    entity->RemoveComponentC(componentPair.second);
+                    itComponents = entity->RemoveComponentC(itComponents->second.get());
                 }
 
-                for (ConsoleGraphX_Interal::Component* comp : entity->GetScripts())
+                auto& scripts = entity->GetScripts();
+                for(auto itScripts = scripts.begin(); itScripts != scripts.end();)
                 {
-                    Script* script = static_cast<Script*>(comp);
-                    entity->RemoveComponentC(script);
+                    itScripts = entity->RemoveComponentC(itScripts->second.get());
                 }
 
+                // Erase the entity from the scene
                 _m_entities.erase(it);
 
+                // Delete the entity itself
                 delete entity;
             }
         }
+
         this->_m_entitiesToDelete.clear();
     }
 
