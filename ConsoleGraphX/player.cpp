4df6f75847a17b4ed9cec8377eaed82fe4103052
Curtidor <tannermatos18@gmail.com>
2024-05-19 21:05:29 -0300

    code clean up

--- ConsoleGraphX/player.cpp
+++ ConsoleGraphX/player.cpp
@@ -17,13 +17,13 @@ Player::Player() : Script(), _m_playerCam(nullptr)
 
 void Player::Awake(Entity* owner)
 {
+	_m_playerCam = owner->GetComponent<Camera>();
+
 	Sprite* sprite = (Sprite*)owner->AddComponent<Sprite>(7, 12, DarkYellow);
 	sprite->m_layer = 10;
 
 	owner->GetComponent<Transform>()->SetPosition(0,40);
-	owner->AddComponent<PlayerController>(25, 5, 9);
-
-	_m_playerCam = owner->GetComponent<Camera>();
+	owner->AddComponent<PlayerController>(100, 5, 9);
 }
 
 void Player::Update(Entity* owner)
@@ -32,15 +32,12 @@ void Player::Update(Entity* owner)
 	Vector3 camPostion = _m_playerCam->GetPosition();
 
 	_m_playerCam->SetPosition(Vector3(playerPosition.x - _m_playerCam->GetWidth() * 0.5f, playerPosition.y - _m_playerCam->GetHeight() * 0.5f));
-	//_m_playerCam->SetPosition(Vector3(playerPosition.x - _m_playerCam->GetWidth() * 0.5f, 0));
 
 	ConsoleGraphX_Interal::Debugger::S_LogMessage("player x: " + std::to_string(playerPosition.x) + " y: " + std::to_string(playerPosition.y));
 	ConsoleGraphX_Interal::Debugger::S_LogMessage("cam x: " + std::to_string(camPostion.x) + " y: " + std::to_string(camPostion.y));
 
 	if (InputSystem::IsKeyPressed(Key::B))
-	{
 		owner->KillEntity();
-	}
 }
 
 
