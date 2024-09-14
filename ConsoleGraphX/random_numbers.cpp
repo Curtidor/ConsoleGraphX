d447f88956c28cfbbcd5072134aaed62d683fe80
Curtidor <tannermatos18@gmail.com>
2024-09-14 20:20:32 -0300

    improved performance in RNG generation by using a more efficient engine

--- ConsoleGraphX/random_numbers.cpp
+++ ConsoleGraphX/random_numbers.cpp
@@ -1,20 +1,12 @@
-#include "random_numbers.h"
 #include <random>
+#include "random_numbers.h"
 
 int RandomNumberGenerator::GenerateRandomIntInRange(int min, int max) {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> dis(min, max);
-
-	return dis(gen);
+    std::uniform_int_distribution<> dis(min, max);
+    return dis(gen);
 }
 
-
-
 float RandomNumberGenerator::GenerateRandomFloatInRange(float min, float max) {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_real_distribution<float> dis(min, max);
-
-	return dis(gen);
+    std::uniform_real_distribution<float> dis(min, max);
+    return dis(gen);
 }
--- ConsoleGraphX/random_numbers.h
+++ ConsoleGraphX/random_numbers.h
@@ -1,9 +1,14 @@
 #pragma once
+#include <random>
 
-class RandomNumberGenerator 
-{
+class RandomNumberGenerator {
 public:
-	static int GenerateRandomIntInRange(int min, int max);
-	static float GenerateRandomFloatInRange(float min, float max);
-		
+    // Generates a random integer in the range [min, max]
+    static int GenerateRandomIntInRange(int min, int max);
+
+    // Generates a random float in the range [min, max]
+    static float GenerateRandomFloatInRange(float min, float max);
+
+private:
+    static inline std::minstd_rand gen = std::minstd_rand(std::random_device{}());
 };
