205e1bb9fd49c34ee0269ba29b66586d1798a4c1
Curtidor <tannermatos18@gmail.com>
2024-09-14 20:25:08 -0300

    redesigned scripts to work with the new component system

--- ConsoleGraphX/script.cpp
+++ ConsoleGraphX/script.cpp
@@ -1,23 +1,51 @@
+#include <stdexcept>
 #include "script.h"
-#include "component.h"
 #include "entity.h"
+#include "verify_macro.h"
 
 namespace ConsoleGraphX
 {
-	Script::Script() : _m_isEnabled(true)
+	Script::Script() : _m_isEnabled(true), _m_owner(nullptr)
 	{}
 
-	Script::Script(const Script& script) : _m_isEnabled(true)
-	{}
-
-	void Script::Clone(Script*& script) const
+	Script::Script(Entity* owner) : _m_isEnabled(true), _m_owner(owner)
 	{
-		script = new Script(*this);
+		CGX_VERIFY(owner);
 	}
 
-	void Script::Awake(Entity* owner) {}
-	void Script::Start(Entity* owner) {}
-	void Script::Update(Entity* owner) {}
+	Script::Script(const Script& script, Entity* owner) : _m_isEnabled(script._m_isEnabled), _m_owner(owner)
+	{}
+
+	void Script::Clone(Script*& script)
+	{
+		script = new Script(*this, script->_m_owner);
+	}
+
+	void Script::Clone(Script*& script, Entity* owner)
+	{
+		script = new Script(*this, owner);
+	}
+
+	void Script::SetOwner(Entity* owner)
+	{
+		if (owner != nullptr && _m_owner == nullptr)
+		{
+			_m_owner = owner;
+		}
+		else if (_m_owner != nullptr)
+		{
+			throw std::runtime_error("cannot reassign owning entity");
+		}
+	}
+
+	const Entity* Script::GetOwner() const
+	{
+		return _m_owner;
+	}
+
+	void Script::Awake() {}
+	void Script::Start() {}
+	void Script::Update(float deltaTime) {}
 
 	bool Script::IsEnabled() const
 	{
