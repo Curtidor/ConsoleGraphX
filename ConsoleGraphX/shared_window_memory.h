094f1a95780f4e9c0032a88ae42ad909c3bd03af
Curtidor <tannermatos18@gmail.com>
2024-10-26 16:16:30 -0300

    header change

--- ConsoleGraphX/shared_window_memory.h
+++ ConsoleGraphX/shared_window_memory.h
@@ -1,19 +1,24 @@
 #pragma once
-#include "CGXPCH.h" // Ensure the necessary headers are included
+#include "PCH_CGX.h" 
 #include <bitset>
 
-// Implemented in header as other projects use this code
+// WARNING: This structure is intended to be used **only** in the Window class and WindowHandler program for a very specific use case.
+// It is tightly coupled to the shared memory design of the windows and should not be reused elsewhere.
 struct SharedWindowMemory
 {
     size_t m_bufferSize;
     std::bitset<255> m_pressedKeys;
 
-    CHAR_INFO m_buffer[]; // Actual buffer will be allocated with the shared memory
+    // change to a one-element array, with the rest allocated later
+    CHAR_INFO m_buffer[1];  // flexible array, actual size determined at runtime
 
-    SharedWindowMemory(size_t bufferSize) : m_bufferSize(bufferSize)
-    {
-        // m_buffer will start immediately after SharedWindowMemory in shared memory
-    }
+    // disable the warning for uninitialized members specifically for this structure
+#pragma warning(push)
+#pragma warning(disable : 26495)
+
+    SharedWindowMemory(size_t bufferSize) : m_bufferSize(bufferSize) {}
 
     ~SharedWindowMemory() = default;
+
+#pragma warning(pop)
 };
