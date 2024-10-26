14d60e1852e095f43b15d3698cf8dba37e6db696
Curtidor <tannermatos18@gmail.com>
2024-10-26 16:17:39 -0300

    engine window

---
+++ ConsoleGraphX/engine_window.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "window.h"
+
+namespace ConsoleGraphX
+{
+    class EngineWindow : public Window
+    {
+    public:
+        EngineWindow(short width, short height, const std::string& windowName, short fontWidth = 3, short fontHeight = 3)
+            : Window(width, height, windowName, fontWidth, fontHeight)
+        {}
+
+        WindowType GetType() const override
+        {
+            return WindowType::EngineCreated;
+        }
+
+    };
+};
