c3b21a42744a3ab7786168843f2258ef135d463b
Curtidor <tannermatos18@gmail.com>
2024-10-26 16:09:29 -0300

    header clean up

--- ConsoleGraphX/engine.cpp
+++ ConsoleGraphX/engine.cpp
@@ -1,34 +1,36 @@
-#include "CGXPCH.h"
+#include "PCH_CGX.h"
+#include "WinCore.h"
 #include "engine.h"
-#include "logger.h"
+// managers
 #include "system_manager.h"
+#include "logger_manager.h"
+// systems
 #include "input_system.h"
 #include "player_controller_system.h"
+#include "script_system.h"
+//graphics
 #include "renderer.h"
 #include "screen.h"
-#include "script_system.h"
+//others
 #include "logger.h"
-#include "debugger_manager.h"
-#include "script_system.h"
-#include "window_manager.h"
+
 
 namespace ConsoleGraphX
 {
-    Engine::Engine(short screen_width, short screen_height, short pixel_width, short pixel_height)
+    Engine::Engine(short screenWidth, short screenHeight, short pixelWidth, short pixelHeight)
         : _m_logger(ConsoleGraphX_Internal::Logger("Engine")), _m_systemManager(ConsoleGraphX_Internal::SystemManager()), 
-        _m_screen(ConsoleGraphX_Internal::Screen(screen_width, screen_height, pixel_width, pixel_height))
+        _m_screen(ConsoleGraphX_Internal::Screen(screenWidth, screenHeight, pixelWidth, pixelHeight))
     {
-        ConsoleGraphX_Internal::Screen::SetActiveScreen_A(&_m_screen);
-
         _m_systemManager.RegisterSystem<ScriptSystem>();
         _m_systemManager.RegisterSystem<PlayerControllerSystem>();
 
+
+        ConsoleGraphX_Internal::Screen::SetActiveScreen_A(&_m_screen);
     }
 
     void Engine::Initialize()
     {
-        ConsoleGraphX_Internal::DebuggerManager::Initialize();
-        WindowManager::Initialize();
+        ConsoleGraphX_Internal::LoggerManager::Initialize();
     }
 
     void Engine::WarmUp()
@@ -44,8 +46,7 @@ namespace ConsoleGraphX
     void Engine::Shutdown()
     {
         _m_isRunning = false;
-        WindowManager::ShutDown();
-        ConsoleGraphX_Internal::DebuggerManager::ShutDown(); // should be the last thing to close
+        ConsoleGraphX_Internal::LoggerManager::ShutDown(); // should be the last thing to close
     }
 
     bool Engine::IsRunning() const
@@ -61,14 +62,15 @@ namespace ConsoleGraphX
 
     void Engine::Render(float alpha)
     {
-        _m_screen.FillScreen(CHAR_INFO{ _m_screen.s_pixel, 0 });
+        _m_screen.FillCanvas(CHAR_INFO{ _m_screen.s_pixel, 0 });
         ConsoleGraphX_Internal::Renderer::DrawSprites(alpha);
         _m_screen.DrawScreen();
+
     }
 
     void Engine::UpdateFPS(int fps)
     {
-        _m_screen.SetConsoleName("FPS: " + std::to_string(fps));
+        SetConsoleNameWC("FPS: " + std::to_string(fps));
     }
 
 };
