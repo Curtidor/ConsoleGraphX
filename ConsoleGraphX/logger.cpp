968d3715f6d0fc00f4341507a5ef58c816bc2fb4
Curtidor <tannermatos18@gmail.com>
2024-10-26 16:15:11 -0300

    renamed from debugger to logger

--- ConsoleGraphX/logger.cpp
+++ ConsoleGraphX/logger.cpp
@@ -1,6 +1,6 @@
-#include "CGXPCH.h"
+#include "PCH_CGX.h"
 #include "logger.h"
-#include "debugger_manager.h"
+#include "logger_manager.h"
 
 namespace ConsoleGraphX_Internal
 {
@@ -8,9 +8,9 @@ namespace ConsoleGraphX_Internal
 	Logger::Logger(const std::string& name): _m_name(name)
 	{}
 
-	void Logger::LogMessage(const std::string& message, DebuggerManager::LogLevel level) const
+	void Logger::LogMessage(const std::string& message, LoggerManager::LogLevel level) const
 	{
-		DebuggerManager::Instance().LogMessage(_m_name, message, level);
+		LoggerManager::Instance().LogMessage(_m_name, message, level);
 	}
 };
 
