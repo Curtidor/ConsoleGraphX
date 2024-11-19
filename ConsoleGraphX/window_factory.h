44f1cd49cc30b691c224a24ab1f4f97e4c79ee22
Curtidor <tannermatos18@gmail.com>
2024-11-18 21:43:47 -0400

    removed files

--- ConsoleGraphX/window_factory.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "window.h"
-
-namespace ConsoleGraphX_Internal
-{
-    std::unique_ptr<ConsoleGraphX::Window> CreateUserWindow(short width, short height, const std::string& windowName, short fontWidth = 3, short fontHeight = 3);
-    std::unique_ptr<ConsoleGraphX::Window> CreateEngineWindow(short width, short height, const std::string& windowName, short fontWidth = 3, short fontHeight = 3);
-};
