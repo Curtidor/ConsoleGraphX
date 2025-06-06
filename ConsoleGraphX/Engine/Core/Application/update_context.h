0675dfd366b0be31089ebabf71a2d70b52ec3b7f
Curtidor <tannermatos18@gmail.com>
2025-06-05 20:36:09 -0700

    Add Application* to UpdateContext for MIN_BUILD Allows passing app reference in minimal build mode.

--- ConsoleGraphX/Engine/Core/Application/update_context.h
+++ ConsoleGraphX/Engine/Core/Application/update_context.h
@@ -1,8 +1,13 @@
 #pragma once
 #include "./Engine/Systems/input_system.h"
+//#include "./Engine/Core/Application/application.h"
+
 
 namespace ConsoleGraphX
 {
+#if MIN_BUILD == 1
+	class Application;
+#endif
 	class SceneSystem; // sorry
 
 	struct UpdateContext
@@ -10,5 +15,9 @@ namespace ConsoleGraphX
 		SceneSystem& sceneSystem;
 		float deltaTime;
 		Input input;
+	#if MIN_BUILD == 1
+		Application* app;
+	#endif
+	
 	};
 }
